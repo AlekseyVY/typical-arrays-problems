1bf54451364b70809959679c2b0b91a0a48fb45f
Aleksey Vasiliev <alekseyvy@gmail.com>
2020-12-19 10:49:41 +0300

    feature: task completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length === 0) {
+    return 0
+  } else {
+    const newArr = array.sort((a, b) => a - b)
+    return newArr[0];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length === 0) {
+    return 0
+  } else {
+    const newArr = array.sort((a, b) => a - b)
+    return newArr[newArr.length - 1];
+  }
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length === 0) {
+    return 0
+  } else {
+    let sum = array.reduce((acc, val) => acc + val, 0)
+    return sum / array.length;
+  }
 }
